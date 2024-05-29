eddd877ff372b0a7b29fb342c32d9fbe791b6e7b
valve16 <glushkovoleg2004@yandex.ru>
2024-05-29 20:06:36 +0300

    split h and cpp

--- lw7/CMyList/CMyList/CMyList.h
+++ lw7/CMyList/CMyList/CMyList.h
@@ -1,4 +1,10 @@
-#pragma once
+#ifndef CMYLIST_H
+#define CMYLIST_H
+#include <iostream>
+#include <stdexcept>
+#include <iterator>
+#include <utility>
+
 template<typename T>
 class CMyList 
 {
@@ -12,120 +18,24 @@ private:
         Node(T&& value) : data(std::move(value)), prev(nullptr), next(nullptr) {}
     };
 public:
-    CMyList() : m_head(nullptr), m_tail(nullptr), m_size(0) {}
+    CMyList();
+    CMyList(const CMyList& list);
+    CMyList(CMyList&& list) noexcept;
 
-    CMyList(const CMyList& list) : m_head(nullptr), m_tail(nullptr), m_size(0) 
-    {
-        CopyFrom(list);
-    }
+    CMyList& operator=(const CMyList& list);
+    CMyList& operator=(CMyList&& list) noexcept;
 
-    CMyList(CMyList&& list) noexcept : m_head(list.m_head), m_tail(list.m_tail), m_size(list.m_size) 
-    {
-        list.m_head = nullptr;
-        list.m_tail = nullptr;
-        list.m_size = 0;
-    }
+    ~CMyList() noexcept;
 
-    CMyList& operator=(const CMyList& list) 
-    {
-        if (this != &list) 
-        {
-            Clear();
-            CopyFrom(list);
-        }
-        return *this;
-    }
+    void push_front(const T& value);
+    void push_front(T&& value);
 
-    CMyList& operator=(CMyList&& list) noexcept 
-    {
-        if (this != &list) 
-        {
-            Clear();
-            m_head = list.m_head;
-            m_tail = list.m_tail;
-            m_size = list.m_size;
-            list.m_head = nullptr;
-            list.m_tail = nullptr;
-            list.m_size = 0;
-        }
-        return *this;
-    }
+    void push_back(const T& value);
+    void push_back(T&& value);
 
-    ~CMyList() noexcept 
-    {
-        Clear();
-    }
+    size_t GetSize() const noexcept;
 
-    void push_front(const T& value) 
-    {
-        Node* newNode = new Node(value);
-        newNode->next = m_head;
-        if (m_head) 
-        {
-            m_head->prev = newNode;
-        }
-        else 
-        {
-            m_tail = newNode;
-        }
-        m_head = newNode;
-        ++m_size;
-    }
-
-    void push_front(T&& value) 
-    {
-        Node* newNode = new Node(std::move(value));
-        newNode->next = m_head;
-        if (m_head) 
-        {
-            m_head->prev = newNode;
-        }
-        else 
-        {
-            m_tail = newNode;
-        }
-        m_head = newNode;
-        ++m_size;
-    }
-
-    void push_back(const T& value) 
-    {
-        Node* newNode = new Node(value);
-        newNode->prev = m_tail;
-        if (m_tail) 
-        {
-            m_tail->next = newNode;
-        }
-        else 
-        {
-            m_head = newNode;
-        }
-        m_tail = newNode;
-        ++m_size;
-    }
-
-    void push_back(T&& value) 
-    {
-        Node* newNode = new Node(std::move(value));
-        newNode->prev = m_tail;
-        if (m_tail) 
-        {
-            m_tail->next = newNode;
-        }
-        else 
-        {
-            m_head = newNode;
-        }
-        m_tail = newNode;
-        ++m_size;
-    }
-
-    size_t getSize() const noexcept 
-    {
-        return m_size;
-    }
-
-    class iterator 
+    class CIt
     {
     private:
         Node* m_current;
@@ -136,46 +46,26 @@ public:
         using pointer = T*;
         using reference = T&;
 
-        explicit iterator(Node* node) : m_current(node) {}
+        explicit CIt(Node* node);
 
-        reference operator*() const { return m_current->data; }
-        pointer operator->() { return &(m_current->data); }
+        reference operator*() const;
+        pointer operator->();
 
-        iterator& operator++() 
-        {
-            m_current = m_current->next;
-            return *this;
-        }
+        CIt& operator++();
+        CIt operator++(int);
 
-        iterator operator++(int) 
-        {
-            iterator tmp = *this;
-            m_current = m_current->next;
-            return tmp;
-        }
+        CIt& operator--();
+        CIt operator--(int);
 
-        iterator& operator--() 
-        {
-            m_current = m_current->prev;
-            return *this;
-        }
-
-        iterator operator--(int) 
-        {
-            iterator tmp = *this;
-            m_current = m_current->prev;
-            return tmp;
-        }
-
-        friend bool operator==(const iterator& a, const iterator& b) { return a.m_current == b.m_current; }
-        friend bool operator!=(const iterator& a, const iterator& b) { return a.m_current != b.m_current; }
+        friend bool operator==(const CIt& a, const CIt& b) { return a.m_current == b.m_current; }
+        friend bool operator!=(const CIt& a, const CIt& b) { return a.m_current != b.m_current; }
         Node* getNode() const { return m_current; }
     };
 
-    iterator begin() { return iterator(m_head); }
-    iterator end() { return iterator(m_tail->next); }
+    CIt begin();
+    CIt end();
 
-    class reverse_iterator 
+    class CRevIt
     {
     private:
         Node* m_current;
@@ -186,163 +76,40 @@ public:
         using pointer = T*;
         using reference = T&;
 
-        explicit reverse_iterator(Node* node) : m_current(node) {}
+        explicit CRevIt(Node* node);
 
-        reference operator*() const { return m_current->data; }
-        pointer operator->() { return &(m_current->data); }
+        reference operator*() const;
+        pointer operator->();
 
-        reverse_iterator& operator++() 
-        {
-            m_current = m_current->prev;
-            return *this;
-        }
+        CRevIt& operator++();
 
-        reverse_iterator operator++(int)
-        {
-            reverse_iterator tmp = *this;
-            m_current = m_current->prev;
-            return tmp;
-        }
+        CRevIt operator++(int);
 
-        reverse_iterator& operator--() 
-        {
-            m_current = m_current->next;
-            return *this;
-        }
+        CRevIt& operator--();
 
-        reverse_iterator operator--(int)
-        {
-            reverse_iterator tmp = *this;
-            m_current = m_current->next;
-            return tmp;
-        }
+        CRevIt operator--(int);
 
-        friend bool operator==(const reverse_iterator& a, const reverse_iterator& b) { return a.m_current == b.m_current; }
-        friend bool operator!=(const reverse_iterator& a, const reverse_iterator& b) { return a.m_current != b.m_current; }
+        friend bool operator==(const CRevIt& a, const CRevIt& b) { return a.m_current == b.m_current; }
+        friend bool operator!=(const CRevIt& a, const CRevIt& b) { return a.m_current != b.m_current; }
         Node* getNode() const { return m_current; }
     };
 
-    reverse_iterator rbegin() { return reverse_iterator(m_tail); }
-    reverse_iterator rend() { return reverse_iterator(m_tail->next); }
+    CRevIt rbegin();
+    CRevIt rend();
 
-    iterator insert(iterator pos, const T& value) 
-    {
-        Node* posNode = pos.getNode();
-        Node* newNode = new Node(value);
+    CIt insert(CIt pos, const T& value);
+    CIt insert(CIt pos, T&& value);
 
-        if (!posNode) 
-        {
-            push_back(value);
-            return iterator(m_tail);
-        }
-
-        newNode->next = posNode;
-        newNode->prev = posNode->prev;
-
-        if (posNode->prev) 
-        {
-            posNode->prev->next = newNode;
-        }
-        else 
-        {
-            m_head = newNode;
-        }
-
-        posNode->prev = newNode;
-
-        ++m_size;
-        return iterator(newNode);
-    }
-
-    iterator insert(iterator pos, T&& value) 
-    {
-        Node* posNode = pos.getNode();
-        Node* newNode = new Node(std::move(value));
-        newNode->next = posNode;
-        if (posNode) 
-        {
-            newNode->prev = posNode->prev;
-            if (posNode->prev) 
-            {
-                posNode->prev->next = newNode;
-            }
-            else 
-            {
-                m_head = newNode;
-            }
-            posNode->prev = newNode;
-        }
-        else 
-        {
-            newNode->prev = m_tail;
-            if (m_tail) 
-            {
-                m_tail->next = newNode;
-            }
-            else 
-            {
-                m_head = newNode;
-            }
-            m_tail = newNode;
-        }
-        ++m_size;
-        return iterator(newNode);
-    }
-
-    iterator erase(iterator pos) 
-    {
-        Node* posNode = pos.getNode();
-        if (!posNode) 
-        {
-            throw std::out_of_range("Invalid iterator position");
-        }
-
-        if (posNode->prev) 
-        {
-            posNode->prev->next = posNode->next;
-        }
-        else 
-        {
-            m_head = posNode->next;
-        }
-
-        if (posNode->next) 
-        {
-            posNode->next->prev = posNode->prev;
-        }
-        else 
-        {
-            m_tail = posNode->prev;
-        }
-
-        iterator next(posNode->next);
-        delete posNode;
-        --m_size;
-        return next;
-    }
-private:
-
-    void Clear() noexcept 
-    {
-        while (m_head) 
-        {
-            Node* tmp = m_head;
-            m_head = m_head->next;
-            delete tmp;
-        }
-        m_tail = nullptr;
-        m_size = 0;
-    }
-
-    void CopyFrom(const CMyList& list) 
-    {
-        for (Node* cur = list.m_head; cur != nullptr; cur = cur->next) 
-        {
-            push_back(cur->data);
-        }
-    }
+    CIt erase(CIt pos);
 private:
     Node* m_head;
     Node* m_tail;
     size_t m_size;
+
+    void CopyFrom(const CMyList& list);
+    void Clear() noexcept;
 };
+
+#include "CMyList.cpp"
+
+#endif 
--- lw7/CMyList/CMyList/main.cpp
+++ lw7/CMyList/CMyList/main.cpp
@@ -1,7 +1,4 @@
-﻿// CMyList.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include "CMyList.h"
 
 int main()
